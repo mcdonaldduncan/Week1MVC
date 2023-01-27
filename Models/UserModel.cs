78ecdf85d8e1e57673b7df555af1c2b704b010b5
Duncan McDonald <72177724+mcdonaldduncan@users.noreply.github.com>
2023-01-26 18:21:52 -0600

    Initial commit

---
+++ Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Week1MVC.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+
+    }
+}
